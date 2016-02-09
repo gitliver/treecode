51c27e81e38a1e8a823d0001423839f8ab042957
Guido D'Amico <gudamico@macth82.cern.ch>
2016-02-09 13:25:11 +0100

    Cleaned and working

--- final.py
+++ final.py
@@ -7,6 +7,7 @@ from scipy import stats, optimize
 
 import numpy as np
 import helpers
+from save_data import Counts
 import os
 
 
@@ -47,7 +48,7 @@ def plot_AD(data):
     # With KS, I checked that using empirical KS test is the same, so I use it here as well
     counts = data.counts
     Ncat = data.Ncat
-    fiducial = data.generate_mc()
+    fiducial = data.generate_mc(100)
 
     ln_par = data.lognorm_par()
     ad_ln = helpers.my_ad([counts, np.random.lognormal(np.log(ln_par[2]), ln_par[0], size=Ncat)])
@@ -70,7 +71,7 @@ def plot_AD(data):
 def plot_KS(data):
     """KS test, given a Counts object"""
     counts = data.counts
-    fiducial = data.generate_mc()
+    fiducial = data.generate_mc(100)
 
     ks_ln = stats.kstest(counts, 'lognorm', args=data.lognorm_par())
     ks_data = [stats.ks_2samp(counts, mc)[0] for mc in fiducial]
@@ -83,7 +84,7 @@ def plot_KS(data):
     plt.axvline(ks_ln[0], c='Orange', label = 'Lognormal')
     plt.legend(loc='best')
     # plt.savefig(os.path.join('all_data', 'KS_'+name+'.png'))
-    return
+    return fig
 
 
 def plot_KL(data):
@@ -91,14 +92,14 @@ def plot_KL(data):
     The 'true' distribution is the data one"""
     frequencies = data.frequencies
     Ncat = data.Ncat
-    fiducial = data.generate_mc()
+    fiducial = data.generate_mc(100)
 
     sh, loc, sc = data.lognorm_par()
     freq_ln = [np.sort(stats.lognorm.rvs(sh, scale=sc, size=Ncat, random_state=s))[::-1]
                for s in range(1, 1001)]
     kl_ln = [stats.entropy(frequencies, r) for r in freq_ln]
 
-    lengths = [min(Ncat, len(mc)) for mc in data.fiducial]  # Cut to the minimum Ncat
+    lengths = [min(Ncat, len(mc)) for mc in fiducial]  # Cut to the minimum Ncat
     kl_data = [stats.entropy(frequencies[:lengths[i]], mc[:lengths[i]]) for i, mc in enumerate(fiducial)]
 
     # Plot KL divergence. Use kdeplot instead of histogram
@@ -121,7 +122,8 @@ def plot_loglog(data):
     counts = data.counts
     Ncat = data.Ncat
     ranks = data.ranks
-    fiducial = data.generate_mc()
+    fiducial = data.generate_mc(100)
+    cdf = data.get_cdf()
 
     fig = plt.figure(figsize=[10, 6.18])
     ax = fig.gca()
@@ -138,17 +140,17 @@ def plot_loglog(data):
             's', ms=3, color='Orange', rasterized=True,
             label='Data')
 
-    ax.plot(data.cdf[0], Ncat*data.cdf[1],
+    ax.plot(cdf[0], Ncat*cdf[1],
             ls='--', color='Magenta', linewidth=3, label='Tree distr.')
 
-    ax.plot(counts, Ncat*data.lognorm_fit(),
+    ax.plot(counts, Ncat*data.lognorm_sf(),
             ls='-.', color='Blue', linewidth=3, label='Lognormal')
 
     par = fit_bpl(data)
     ax.plot(np.sort(counts), Ncat*bpl_sf(np.sort(counts), *par),
             ls=':', color='Crimson', linewidth=3, label='Broken PL')
 
-    avg = helpers.avg_mc(data.fiducial, data.counts)
+    avg = helpers.avg_mc(fiducial, counts)
     Nc = len(avg)
     ax.plot(avg, np.arange(1, Nc+1),
             ls='-', color='Lime', linewidth=3, label='Average')
@@ -161,17 +163,16 @@ def plot_loglog(data):
     brown_patch = mpatches.Patch(color='Crimson', label='Broken PL')
     plt.legend(handles=[gray_patch, orange_patch, green_patch,
                         magenta_patch, blue_patch, brown_patch])
-
-    # plt.savefig(os.path.join('all_data', 'new_loglog_'+data.name+'.png'))
-    return
+    return fig
 
 
-def plot_loglog(data):
+def plot_loglin(data):
     """Plot data, given a Counts object"""
     counts = data.counts
     Ncat = data.Ncat
     ranks = data.ranks
-    fiducial = data.generate_mc()
+    fiducial = data.generate_mc(100)
+    cdf = data.get_cdf()
 
     fig = plt.figure(figsize=[10, 6.18])
     ax = fig.gca()
@@ -187,17 +188,17 @@ def plot_loglog(data):
             's', ms=3, color='Orange', rasterized=True,
             label='Data')
 
-    ax.plot(data.cdf[0], Ncat*data.cdf[1],
+    ax.plot(cdf[0], Ncat*cdf[1],
             ls='--', color='Magenta', linewidth=3, label='Tree distr.')
 
-    ax.plot(counts, Ncat*data.lognorm_fit(),
+    ax.plot(counts, Ncat*data.lognorm_sf(),
             ls='-.', color='Blue', linewidth=3, label='Lognormal')
 
     par = fit_bpl(data)
     ax.plot(np.sort(counts), Ncat*bpl_sf(np.sort(counts), *par),
             ls=':', color='Crimson', linewidth=3, label='Broken PL')
 
-    avg = helpers.avg_mc(data.fiducial, data.counts)
+    avg = helpers.avg_mc(fiducial, counts)
     Nc = len(avg)
     ax.plot(avg, np.arange(1, Nc+1),
             ls='-', color='Lime', linewidth=3, label='Average')
@@ -210,47 +211,46 @@ def plot_loglog(data):
     brown_patch = mpatches.Patch(color='Crimson', label='Broken PL')
     plt.legend(handles=[gray_patch, orange_patch, green_patch,
                         magenta_patch, blue_patch, brown_patch])
-
-    # plt.savefig(os.path.join('all_data', 'new_loglin_'+data.name+'.png'))
-    return
+    return fig
 
 
 def main():
-    names = ('amazon', 'amazon_new', 'arxiv', 'bacteria', 'cath', 'counties',
-             'files', 'germanpop', 'harvard', 'hospital', 'merchants',
-             'microbiome', 'mta', 'NATC', 'quora',
-             'pop2014', 'raul', 'reddit', 'swisspop')
-    names = ('arxiv2',)
+    names = ('sample',)
     sigma_Ncat = np.empty((len(names), 4))
-    #sigma_Ncat = pickle.load(open('sigma_Ncat.pkl', 'rb'))
 
     for i, name in enumerate(names):
         print('\nDoing '+name+'.....')
-        data = Dataset(os.path.join(dirdata, name+'.pkl'))
+        data = Counts(os.path.join(os.curdir, 'counts_'+name+'.dat'))
 
         counts = data.counts
         ranks = data.ranks
-
         Ncat = data.Ncat
         Nitems = data.Nitems
-        print("Ncat = ", Ncat, "\tNitems = ", Nitems)
-        print("factor = ", data.factor)
 
-        sigma_Ncat[i, 0] = Ncat
-        sigma_Ncat[i, 1] = data.lognorm_par()[0]
-        sigma_Ncat[i, 2] = np.var(np.log(counts))
-        # Check that we have all non-zero categories
-        print("Categories with zero count:",
-              np.sum([m[m == 0].shape for m in data.fiducial]))
-        sigma_Ncat[i, 3] = np.mean([np.var(np.log(m)) for m in data.fiducial])
+        factor = data.get_factor()
+        print("Ncat = ", Ncat, "\tNitems = ", Nitems)
+        print("factor = ", factor)
+
+        # sigma_Ncat[i, 0] = Ncat
+        # sigma_Ncat[i, 1] = data.lognorm_par()[0]
+        # sigma_Ncat[i, 2] = np.var(np.log(counts))
+        # # Check that we have all non-zero categories
+        # print("Categories with zero count:",
+        #       np.sum([m[m == 0].shape for m in data.fiducial]))
+        # sigma_Ncat[i, 3] = np.mean([np.var(np.log(m)) for m in data.fiducial])
 
         chao_est, chao_sigma = helpers.chao(data)
         print('Chao estimator: ', chao_est)
         print('Chao std: ', chao_sigma)
-        print('Our estimated Q: ', data.factor * data.Ncat)
-        print('Ratio Q/Chao: ', data.factor * data.Ncat/chao_est)
+        print('Our estimated Q: ', factor * data.Ncat)
+        print('Ratio Q/Chao: ', factor * data.Ncat/chao_est)
         print('Sigma/Est for Chao: ', chao_sigma/chao_est)
-
+        fig = plot_KL(data)
+        plt.savefig('KL_plot.png')
+        fig = plot_loglog(data)
+        plt.savefig('loglog_plot.png')
+        fig = plot_loglin(data)
+        plt.savefig('loglin_plot.png')
 
 if __name__ == "__main__":
     main()
